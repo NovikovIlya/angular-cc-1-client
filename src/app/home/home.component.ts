f02b6fb05d23d0d7acfa6dfaf2011ba6fbba792e
NovikovIlya <novikov.i.sergeevich@gmail.com>
2024-04-02 14:42:18 +0300

    Почистил код

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,36 +1,39 @@
-import { Component, ViewChild, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProductsService } from '../services/products.service';
-import { Product, Products } from '../../types';
 
 import { CommonModule } from '@angular/common';
-import { Paginator, PaginatorModule } from 'primeng/paginator';
 
-import { ButtonModule } from 'primeng/button';
 import { ApiService } from '../services/api.service';
 import { BooksStore } from '../store/items.store';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    CommonModule,
-  
-  ],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   readonly store = inject(BooksStore);
+
+  user: any = {};
+  name: string | undefined = '';
+  isLoading: boolean = true;
+  joinedDate = '';
+
   constructor(
     private productsService: ProductsService,
     private apiService: ApiService
   ) {}
 
-  user: any = {};
-  name: string | undefined = '';
-  isLoading:boolean = true;
-  joinedDate = ''
-  fetchProducts(name: any) {
+  ngOnInit() {
+    this.apiService.getData().subscribe((name: string) => {
+      this.name = name;
+      this.fetchProducts(name);
+    });
+  }
+
+  fetchProducts(name: string) {
     this.isLoading = true;
     this.productsService
       .getProducts(`https://api.github.com/users/${name}`)
@@ -38,31 +41,25 @@ export class HomeComponent {
         next: (data: any) => {
           this.user = data;
           this.isLoading = false;
-          this.joinedDate = this.localDate.format(new Date(this.user.created_at));
-          console.log(this.user)
+          this.joinedDate = this.localDate.format(
+            new Date(this.user.created_at)
+          );
+          console.log(this.user);
         },
-        
+
         error: (error) => {
           console.log(error);
         },
       });
   }
-  setStore(){
-    this.store.updateQuery('zaza')
+
+  setStore() {
+    this.store.updateQuery('zaza');
   }
+
   localDate = new Intl.DateTimeFormat('en-GB', {
     day: 'numeric',
     month: 'short',
     year: 'numeric',
   });
-  
-  ngOnInit() {
-    
-    this.apiService.getData().subscribe((name: string) => {
-      this.name = name;
-      this.fetchProducts(name);
-      
-    });
-    // this.fetchProducts('ilya');
-  }
 }
